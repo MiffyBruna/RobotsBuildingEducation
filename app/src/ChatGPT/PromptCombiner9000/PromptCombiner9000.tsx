d1dcb572368f28a89f75e153bccbb45e00f0061f
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2023-09-28 02:33:04 -0700

    refactor Prompt9000

--- app/src/ChatGPT/PromptCombiner9000/PromptCombiner9000.tsx
+++ app/src/ChatGPT/PromptCombiner9000/PromptCombiner9000.tsx
@@ -5,37 +5,42 @@ import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { isEmpty } from "lodash";
 import { logEvent } from "firebase/analytics";
 import Patreon from "../Patreon/Patreon";
-import { analytics } from "../../database/firebaseResources";
 import CodeEditor from "../CodeEditor/CodeEditor";
+import { analytics } from "../../database/firebaseResources";
 
-const Container = styled.div`
+const Wrapper = styled.div`
   text-align: left;
 `;
 
-const Title = styled.h2`
+const Heading = styled.h2`
   margin-top: 24px;
 `;
 
-const ResponseBox = styled.div`
+const MessageContainer = styled.div`
   background-color: ${(props) => (props.loading ? "black" : "#2C2C2E")};
   color: white;
   display: ${(props) => (props.loading ? "none" : "flex")};
   justify-content: flex-start;
-  text-align: left;
   padding: 20px;
   min-width: 350px;
   max-width: 600px;
-  border-radius: 30px 30px 30px 0;
-  margin-top: 24px;
-  margin-bottom: 12px;
+  border-radius: 30px;
+  margin: 24px 0 12px 0;
 `;
 
-const ResponseRenderer = ({
-  type,
-  response,
-  patreonObject,
-  isGeneratedDemo,
-}) => {
+const FlexBox = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Advertisement = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const renderContent = (type, response, patreonObject, isGeneratedDemo) => {
   switch (type) {
     case "patreon":
       return (
@@ -46,7 +51,26 @@ const ResponseRenderer = ({
       );
     case "practice":
       return <CodeEditor patreonObject={patreonObject} />;
-    // Add other cases as needed
+    case "demonstrate":
+      if (patreonObject?.hasCode) {
+        return (
+          <SyntaxHighlighter
+            language={patreonObject?.prompts?.demonstrate?.request
+              ?.split(" ")
+              .slice(-1)[0]
+              ?.slice(0, -1)}
+            style={a11yDark}
+            wrapLines={true}
+            wrapLongLines={true}
+            customStyle={{ width: "100%" }}
+          >
+            {response}
+          </SyntaxHighlighter>
+        );
+      } else {
+        return <div>{response}</div>;
+      }
+      break;
     default:
       return <div>{response}</div>;
   }
@@ -56,31 +80,30 @@ export const PromptCombiner9000 = ({
   loadingMessage,
   chatGptResponse,
   patreonObject,
-
+  isDemo,
+  moduleName = "demo",
   isGeneratedDemo,
 }) => {
-  if (isEmpty(patreonObject)) return null;
+  if (isEmpty(patreonObject)) {
+    return null;
+  }
+
+  const { type, response, icon } = chatGptResponse;
 
   return (
-    <Container>
+    <Wrapper>
       {loadingMessage.length < 1 && (
-        <Title>
-          {chatGptResponse.type === "patreon"
-            ? "generate"
-            : chatGptResponse.type.toLowerCase()}{" "}
-          {chatGptResponse?.icon}
-        </Title>
+        <Heading>
+          {type === "patreon" ? "generate" : type} {icon}
+        </Heading>
       )}
-      <ResponseBox loading={loadingMessage.length >= 1}>
-        <div style={{ display: "flex", width: "100%" }}>
-          <ResponseRenderer
-            type={chatGptResponse.type}
-            response={chatGptResponse.response}
-            patreonObject={patreonObject}
-            isGeneratedDemo={isGeneratedDemo}
-          />
-        </div>
-      </ResponseBox>
-    </Container>
+      <MessageContainer loading={loadingMessage}>
+        <FlexBox>
+          {loadingMessage.length < 1 &&
+            response &&
+            renderContent(type, response, patreonObject, isGeneratedDemo)}
+        </FlexBox>
+      </MessageContainer>
+    </Wrapper>
   );
 };
--- app/src/ChatGPT/Prompts/Prompts.tsx
+++ app/src/ChatGPT/Prompts/Prompts.tsx
@@ -1,167 +1,108 @@
-import { logEvent } from "firebase/analytics";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import { isEmpty } from "lodash";
 import ReactJson from "react-json-view";
+import { logEvent } from "firebase/analytics";
 import { analytics } from "../../database/firebaseResources";
-import { ProofOfWork } from "../../ProofOfWork/ProofOfWork";
 import { StyledPromptButton } from "../../styles/lazyStyles";
-// import { DiscordButton } from "./DiscordButton/DiscordButton";
+
+// Reusable Button Component
+const PromptButton = ({ icon, action, type, loading, onClick }) => (
+  <StyledPromptButton
+    tabindex="0"
+    borderHighlight={"#48484a"}
+    style={{ display: loading ? "none" : "flex" }}
+    onClick={onClick}
+  >
+    <a style={{ color: "white" }}>
+      {icon} &nbsp;{action || type}
+    </a>
+  </StyledPromptButton>
+);
+
+// Modal Content as a separate component
+const ModalContent = ({ patreonObject }) => (
+  <>
+    <h3>What is this?</h3>
+    <p>
+      This is for students and teachers who are curious of how the AI is
+      prompted and fine-tuned over time.
+    </p>
+    <ReactJson
+      theme="threezerotwofour"
+      enableClipboard
+      src={patreonObject}
+      quotesOnKeys={false}
+    />
+  </>
+);
 
 export const Prompts = ({ loadingMessage, patreonObject, handleSubmit }) => {
-  if (!isEmpty(patreonObject)) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    let borderHighlight = "#48484a";
-    let promptMap = [
-      <StyledPromptButton
-        tabindex="0"
-        style={{ display: loadingMessage ? "none" : "flex" }}
-        borderHighlight={borderHighlight}
-        loadingMessage={loadingMessage}
-        onClick={(event) => {
-          if (loadingMessage) {
-          } else {
-            handleSubmit(event, patreonObject.prompts["patreon"], "patreon");
-          }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  if (isEmpty(patreonObject)) return null;
+
+  const promptTypes = ["patreon", "guide", "practice", "shop"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "flex-end",
+        flexDirection: "column",
+      }}
+    >
+      {promptTypes.map((type) => {
+        const prompt = patreonObject.prompts[type];
+        if (!prompt) return null;
+        return (
+          <PromptButton
+            key={type}
+            icon={prompt?.icon}
+            action={prompt?.action}
+            type={type}
+            loading={!!loadingMessage}
+            onClick={(e) => !loadingMessage && handleSubmit(e, prompt, type)}
+          />
+        );
+      })}
+
+      <br />
+      <Button
+        variant="dark"
+        onClick={() => {
+          logEvent(analytics, "select_content", {
+            content_type: "button",
+            item_id: "View Roxana",
+          });
+          setIsModalOpen(true);
         }}
       >
-        <a style={{ color: "white" }}>
-          {patreonObject.prompts["patreon"]?.icon} &nbsp;
-          {/* {patreonObject.prompts['patreon'].action} */}
-          discover
-        </a>
-      </StyledPromptButton>,
-      <StyledPromptButton
-        tabindex="0"
-        style={{ display: loadingMessage ? "none" : "flex" }}
-        borderHighlight={borderHighlight}
-        loadingMessage={loadingMessage}
-        onClick={(event) => {
-          if (loadingMessage) {
-          } else {
-            handleSubmit(event, patreonObject.prompts["guide"], "guide");
-          }
-        }}
+        💗 Roxana
+      </Button>
+      <br />
+      <br />
+      <Modal
+        centered
+        fullscreen
+        show={isModalOpen}
+        onHide={() => setIsModalOpen(false)}
       >
-        <a style={{ color: "white" }}>
-          {/* {patreonObject.prompts['guide'].icon}{" "} */}
-          📚 &nbsp;study
-        </a>
-      </StyledPromptButton>,
-      ...(patreonObject.prompts.practice
-        ? [
-            <StyledPromptButton
-              tabindex="0"
-              style={{ display: loadingMessage ? "none" : "flex" }}
-              borderHighlight={borderHighlight}
-              loadingMessage={loadingMessage}
-              onClick={(event) => {
-                if (loadingMessage) {
-                } else {
-                  handleSubmit(
-                    event,
-                    patreonObject.prompts["practice"],
-                    "practice"
-                  );
-                }
-              }}
-            >
-              <a style={{ color: "white" }}>
-                {patreonObject.prompts["practice"]?.icon} &nbsp;
-                {patreonObject.prompts["practice"]?.action}
-              </a>
-            </StyledPromptButton>,
-          ]
-        : []),
-
-      <StyledPromptButton
-        tabindex="0"
-        style={{ display: loadingMessage ? "none" : "flex" }}
-        borderHighlight={borderHighlight}
-        loadingMessage={loadingMessage}
-        onClick={(event) => {
-          if (loadingMessage) {
-          } else {
-            handleSubmit(event, patreonObject.prompts["shop"], "shop");
-          }
-        }}
-      >
-        <a style={{ color: "white" }}>
-          {patreonObject.prompts["shop"]?.icon} &nbsp;
-          {patreonObject.prompts["shop"]?.action}
-        </a>
-      </StyledPromptButton>,
-    ];
-
-    return (
-      <div
-        style={{
-          display: "flex",
-
-          alignItems: "flex-end",
-          flexDirection: "column",
-        }}
-      >
-        {promptMap}
-        <br />
-        <Button
-          variant="dark"
-          onClick={() => {
-            logEvent(analytics, "select_content", {
-              content_type: "button",
-              item_id: "View Roxana",
-            });
-
-            setIsModalOpen(true);
-          }}
+        <Modal.Header
+          closeButton
+          style={{ color: "white", backgroundColor: "black" }}
         >
-          💗 Roxana
-        </Button>
-
-        <br />
-        <br />
-        <Modal
-          centered
-          fullscreen={true}
-          show={isModalOpen}
-          onHide={() => setIsModalOpen(false)}
-        >
-          <Modal.Header
-            closeButton
-            style={{ color: "white", backgroundColor: "black" }}
-          >
-            <Modal.Title>AI Prompt Engineering</Modal.Title>
-          </Modal.Header>
-          <Modal.Body style={{ backgroundColor: "black", color: "white" }}>
-            <div
-              style={{
-                width: "100%",
-                wordBreak: "break-word",
-              }}
-            >
-              <h3>What is this?</h3>
-              <p>
-                This is for students and teachers who are curious of how the AI
-                is prompted and fine-tuned over time.
-              </p>
-
-              <ReactJson
-                theme={"threezerotwofour"}
-                enableClipboard
-                src={patreonObject}
-                quotesOnKeys={false}
-              />
-            </div>
-          </Modal.Body>
-          <Modal.Footer style={{ color: "white", backgroundColor: "black" }}>
-            <Button variant="dark" onClick={() => setIsModalOpen(false)}>
-              Back to app
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  } else {
-    return null;
-  }
+          <Modal.Title>AI Prompt Engineering</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ backgroundColor: "black", color: "white" }}>
+          <ModalContent patreonObject={patreonObject} />
+        </Modal.Body>
+        <Modal.Footer style={{ color: "white", backgroundColor: "black" }}>
+          <Button variant="dark" onClick={() => setIsModalOpen(false)}>
+            Back to app
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
