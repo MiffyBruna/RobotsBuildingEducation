55384827f5d19cdf923b892d22c2bae5bc3d47a1
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2023-10-06 01:57:52 -0700

    logo replacement

--- app/src/Header/Header.tsx
+++ app/src/Header/Header.tsx
@@ -1,16 +1,18 @@
 import { LearnMore } from "./LearnMore/LearnMore";
-import roxanabrand from "../common/media/images/roxanabrand.gif";
+import robe_logo from "../common/media/images/robe_logo.png";
 import { prettyColorPalette } from "../styles/lazyStyles";
 
 export const Header = () => {
   return (
     <div style={{ color: prettyColorPalette.softYellowGlow }}>
-      <img width="125px" src={roxanabrand} />
-      <br />
-      <br />
-      <h2>RO.₿.E</h2>
-      <h3>Robots Building Education</h3>
-
+      <img width="175px" src={robe_logo} style={{ marginTop: "24px" }} />
+      {/* 
+        <br />
+        <br />
+        <h2>RO.₿.E</h2>
+        <h3>Robots Building Education</h3>
+        <br /> 
+      */}
       <br />
       <LearnMore />
     </div>
